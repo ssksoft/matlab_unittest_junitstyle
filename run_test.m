390c561051e88a36330d552776797ffebba29b93
ssk <sasat.s.93@gmail.com>
2020-05-05 09:57:40 +0900

    テスト成否表示用ログを追加 JenkinsからLogParserプラグインで読み取る予定

--- run_test.m
+++ run_test.m
@@ -10,4 +10,13 @@ p = XMLPlugin.producingJUnitFormat(xmlFile);
 
 runner.addPlugin(p)
 results = runner.run(suite);
-table(results)
+% table(results)
+
+results_table = table(results);
+num_failure = sum(results_table.Failed);
+
+if num_failure > 0
+    disp('Some Unit Tests Result: Failed')
+else
+    disp('All Unit Tests Result: Passed')
+end
